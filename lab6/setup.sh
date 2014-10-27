ae5b664155e8921be7df1a6731da7308d1d6fd73
Amol Deshpande <amol@Amols-MacBook-Pro.local>
2014-10-27 00:10:21 -0400

    .

--- lab6/setup.sh
+++ lab6/setup.sh
@@ -5,7 +5,7 @@ curl -O http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.3.0-cdh5.1.2.tar.gz
 tar -xvf hadoop-2.3.0-cdh5.1.2.tar.gz
 echo 'export HADOOP_HOME="${HOME}/cloudera/cdh5.1/hadoop-2.3.0-cdh5.1.2"' >> ~/.profile
 echo 'export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-i386/jre"' >> ~/.profile
-echo "export PATH=${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin:${JAVA_HOME}/bin:${PATH}" >> ~/.profile
+echo 'export PATH=/home/terrapin/cloudera/cdh5.1/hadoop-2.3.0-cdh5.1.2/bin:/home/terrapin/cloudera/cdh5.1/hadoop-2.3.0-cdh5.1.2/sbin:/usr/lib/jvm/java-7-openjdk-i386/jre/bin:${PATH}' >> ~/.profile
 source $HOME/.profile
 cd -
 cp *.xml $HOME/cloudera/cdh5.1/hadoop-2.3.0-cdh5.1.2/etc/hadoop/
